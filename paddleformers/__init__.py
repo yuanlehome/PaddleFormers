d634b77b9b5a63d94f109eea3347ebde03174bd5
Yuanle Liu <yuanlehome@163.com>
2025-09-28 01:19:23 -0700

    Update __init__.py

--- paddleformers/__init__.py
+++ paddleformers/__init__.py
@@ -31,7 +31,7 @@ with suppress(Exception):
 
 # this version is used for develop and test.
 # release version will be added fixed version by setup.py.
-__version__ = "0.2.5.post"
+__version__ = "0.2.6.post"
 if os.getenv(PADDLEFORMERS_STABLE_VERSION):
     __version__ = __version__.replace(".post", "")
 else:
